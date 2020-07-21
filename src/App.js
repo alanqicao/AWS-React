215f87e23fe13bb564eb489b535b4dac325595de
Qi Cao <alanqicao@gmail.com>
2020-07-20 19:16:19 -0600

    home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route,} from 'react-router-dom'
+import Home from './components/homepage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
+    <Route exact path="/" component={Home} /> 
+    
+     );
 }
 
 export default App;
