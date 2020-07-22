d2d08611c59613a3ba76de8990c4f19e194576c2
Qi Cao <alanqicao@gmail.com>
2020-07-22 11:31:02 -0600

    page layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Layout,Header,Navigation,Drawer,Content } from 'react-mdl/lib/Layout';
 import Main from './components/main';
+import {Link} from 'react-router-dom';
 
 
 function App() {
@@ -10,18 +11,18 @@ function App() {
     <Layout>
         <Header title="Title" scroll>
             <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutMe">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutMe">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
