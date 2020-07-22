dff26d986e6f20eb58fa89fb773412310f91ac8a
Qi Cao <alanqicao@gmail.com>
2020-07-22 10:12:00 -0600

    Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Route,} from 'react-router-dom'
-import Home from './components/homepage'
+import { Layout,Header,Navigation,Drawer,Content } from 'react-mdl/lib/Layout';
 
 function App() {
   return (
-    <Route exact path="/" component={Home} /> 
-    
+    <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+</div>
      );
 }
 
--- src/components/homepage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import anime from 'animejs/lib/anime.es.js';
-
-const Home = () => (
-  <h1>Home</h1>
-);
-
-export default Home;
