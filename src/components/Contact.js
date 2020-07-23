44214c513b40271ac9a3610209357742cc97b42a
Qi Cao <alanqicao@gmail.com>
2020-07-22 20:08:55 -0600

    contacthalfpage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class Contact extends Component {
   render(){
     return(
-      <div><h1>Contact Page</h1></div>
+      <div className="contact-body">
+        <Grid className ="contact-grid">
+          <Cell col={6}>
+            <h2>Alan Cao</h2>
+            <img 
+            src ="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+            alt = "avatar"
+            style={{}}
+            />
+          </Cell>
+          <Cell col={6}>half page</Cell>
+        </Grid>
+      </div>
     )
   }
 }
