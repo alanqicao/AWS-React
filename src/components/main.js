b95480709c55ed51346775542ba90699ba5a06a2
Qi Cao <alanqicao@gmail.com>
2020-07-23 16:37:51 -0600

    fix bug

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import LandingPage from './landingPage'
+import AboutMe from './aboutMe';
 import Contact from './contact';
 import Projects from './projects';
 import Resume from './resume';
-import LandingPage from './landingPage'
-import AboutMe from './aboutMe';
 
 const Main =() => (
 
