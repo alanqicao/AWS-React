54b00919e6b7753cbb0f1992b35d4ec5fd73609e
Qi Cao <alanqicao@gmail.com>
2020-07-23 16:25:28 -0600

    fix bug

--- src/components/main.js
+++ src/components/main.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import LandingPage from './Landingpage';
 import AboutMe from './aboutMe';
 import Contact from './contact';
 import Projects from './projects';
 import Resume from './resume';
+import LandingPage from './landingPage'
 
 const Main =() => (
 
   <Switch>
-    <Route exact path ="/" component ={LandingPage}/>
+    <Route path ="/" component = {LandingPage}/>
     <Route path = "/aboutMe" component ={AboutMe}/>
     <Route path = "/contact" component ={Contact}/>
     <Route path = "/projects" component ={Projects}/>
